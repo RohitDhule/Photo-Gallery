e6b48e5625734f8bbff7f67924448abfc9e65701
RohitDhule <dhulerohit970@gmail.com>
2025-01-05 15:12:43 +0530

    Add files via upload

---
+++ img.js
@@ -0,0 +1,11 @@
+var fullImgBox = document.getElementById("fullImgBox");
+var fillImg = document.getElementsById("fullImg");
+
+function openFullImg(pic){
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+
+function closeFullImg(){
+    fullImgBox.style.display = "none";
+}
